79faf65ef449b20a211b5ef5af79648d461ad7c0
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-08 15:55:05 +0000

    User authenitcate unit tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,19 @@
 require 'user'
 
 describe User do
+  let!(:user) { User.create(email: 'jethro@bea.com', password: 'bjjbbjjb')}
 
+  context '#self.authenticate' do
+    it 'rejects non-existent user' do
+      expect(User.authenticate('bea@jethro.com', 'rad')).to eq nil
+    end
+
+    it 'rejects incorrect password, user exists' do
+      expect(User.authenticate('jethro@bea.com', 'lame')).to eq nil
+    end
+  end
+
+    it 'accepts correct password, user exists' do
+      expect(User.authenticate('jethro@bea.com', 'bjjbbjjb')).to eq user
+    end
 end
