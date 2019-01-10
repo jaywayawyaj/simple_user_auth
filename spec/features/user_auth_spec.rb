59e46202fb3f351d3c7c3364a7a8ba168a8e1e41
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-10 09:01:27 +0000

    web helper added for sign in, tests updated and passing

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -28,20 +28,19 @@ RSpec.feature 'User Authentication' do
     let!(:user) { User.create(email: 'test@test.com', password: 'secret123')}
 
     scenario 'A user can signin' do
-      visit '/'
-      click_on 'Sign in'
-      fill_in :email, with: 'test@test.com'
-      fill_in :password, with: 'secret123'
-      click_button 'Sign in'
+      signin
+
+      expect(page).to have_content 'Welcome, test@test.com'
+    end
+
+    scenario 'A user can signin' do
+      signin
 
       expect(page).to have_content 'Welcome, test@test.com'
     end
 
     scenario 'A signed in user can log out' do
-      visit '/signin'
-      fill_in :email, with: 'test@test.com'
-      fill_in :password, with: 'secret123'
-      click_button 'Sign in'
+      signin
       click_button 'Log out'
 
       expect(page.current_path).to eq '/'
